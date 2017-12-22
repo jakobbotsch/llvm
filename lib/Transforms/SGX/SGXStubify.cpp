85532a05d8f3ff688fd5c063b8a5220edfb42681
Jakob Botsch Nielsen <Jakob.Botsch.Nielsen@gmail.com>
2017-12-22 17:48:43 +0100

    Add support for arbitrary insecure-to-secure calls
    Previously there was only support for entering a single secure
    function. This is because OpenSGX only supports a single TCS. Add
    support for entering any amount of function by having a single "switch
    board" function.

--- lib/Transforms/SGX/SGXStubify.cpp
+++ lib/Transforms/SGX/SGXStubify.cpp
@@ -1,4 +1,5 @@
 #include "SGXInternal.h"
+#include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Analysis/CallGraphSCCPass.h"
 #include "llvm/IR/CallSite.h"
@@ -25,6 +26,13 @@ enum ENCLU_LEAF {
   ENCLU_EEXIT = 4,
 };
 
+struct SecureFuncAdapterInfo {
+  uint32_t FuncIndex;
+  Function *OriginalFunction;
+  Function *InsecureAdapter;
+  Type *FrameTy;
+};
+
 struct SGXStubify : public ModulePass {
   static char ID;
   SGXStubify() : ModulePass(ID) {
@@ -34,27 +42,37 @@ struct SGXStubify : public ModulePass {
   bool runOnModule(Module &M) override;
 
 private:
+  Module *M;
   LLVMContext *C;
   Constant *EncTcsGlobal;
   Constant *EncInit;
   Constant *EncEH;
+  Type *Int8Ty;
+  Type *Int32Ty;
+  Type *Int64Ty;
+  Type *Int8PtrTy;
+  Type *VoidTy;
 
-  Function *createAdapter(Function& F);
+  SecureFuncAdapterInfo createInsecureAdapter(Function &F, uint32_t FuncIndex,
+                                              Function &SecureAdapter);
+  void fillSecureAdapter(Function *SecureAdapter,
+                         ArrayRef<SecureFuncAdapterInfo> Adapters);
 };
+}
 
 bool SGXStubify::runOnModule(Module &M) {
+  this->M = &M;
   C = &M.getContext();
+  Int8Ty = Type::getInt8Ty(*C);
+  Int32Ty = Type::getInt32Ty(*C);
+  Int64Ty = Type::getInt64Ty(*C);
+  Int8PtrTy = Type::getInt8PtrTy(*C);
+  VoidTy = Type::getVoidTy(*C);
 
   // LLVMSGX runtime externals
-  Type *Int8Ty = Type::getInt8Ty(*C);
-  Type *VoidTy = Type::getVoidTy(*C);
-  Type *Int8PtrTy = Type::getInt8PtrTy(*C);
-
   EncTcsGlobal =
     M.getOrInsertGlobal("__llvmsgx_enclave_tcs", Int8PtrTy);
-  EncInit = M.getOrInsertFunction("__llvmsgx_enclave_init",
-                                            VoidTy,
-                                            Int8PtrTy);
+  EncInit = M.getOrInsertFunction("__llvmsgx_enclave_init", VoidTy, Int8PtrTy);
   // OpenSGX EH support (TODO: remove the need for this)
   EncEH = M.getOrInsertGlobal("exception_handler", Int8Ty);
 
@@ -64,6 +82,9 @@ bool SGXStubify::runOnModule(Module &M) {
       SecureFuncs.push_back(&F);
   }
 
+  SmallVector<SecureFuncAdapterInfo, 16> AdapterInfos;
+  uint32_t CurrentAdapterIndex = 0;
+  Function *SecureAdapter = nullptr;
   for (Function *F : SecureFuncs) {
     F->setSection(SGX_SECURE_SECTION);
 
@@ -102,14 +123,32 @@ bool SGXStubify::runOnModule(Module &M) {
     if (InsecureCalls.empty())
       continue; // no adapter necessary
 
-    Function *InsecureAdapter = createAdapter(*F);
+    // A secure adapter will be needed, so create it now if necessary.
+    if (SecureAdapter == nullptr) {
+      FunctionType *SecureAdapterFTy =
+        FunctionType::get(VoidTy, {Int64Ty, Int8PtrTy}, false);
+
+      SecureAdapter = Function::Create(SecureAdapterFTy, Function::PrivateLinkage,
+                                       "__llvmsgx_secadapt");
+
+      M.getFunctionList().insert(F->getIterator(), SecureAdapter);
+    }
+
+    SecureFuncAdapterInfo Adapter =
+      createInsecureAdapter(*F, CurrentAdapterIndex, *SecureAdapter);
+    CurrentAdapterIndex++;
+
     for (Instruction *I : InsecureCalls) {
       if (CallInst *Call = dyn_cast<CallInst>(I))
-        Call->setCalledFunction(InsecureAdapter);
+        Call->setCalledFunction(Adapter.InsecureAdapter);
       if (InvokeInst *Invoke = dyn_cast<InvokeInst>(I))
-        Invoke->setCalledFunction(InsecureAdapter);
+        Invoke->setCalledFunction(Adapter.InsecureAdapter);
     }
+
+    AdapterInfos.push_back(Adapter);
   }
+
+  fillSecureAdapter(SecureAdapter, AdapterInfos);
 
   bool AnyGV = false;
   for (GlobalVariable& GV : M.globals()) {
@@ -125,11 +164,8 @@ bool SGXStubify::runOnModule(Module &M) {
 
 // Creates an adapter that when used from an insecure function
 // calls the specified secure function.
-Function *SGXStubify::createAdapter(Function &F) {
-  Type *Int64Ty = Type::getInt64Ty(*C);
-  Type *VoidTy = Type::getVoidTy(*C);
-  Type *Int8PtrTy = Type::getInt8PtrTy(*C);
-
+SecureFuncAdapterInfo SGXStubify::createInsecureAdapter(Function &F, uint32_t FuncIndex,
+                                                        Function &SecureAdapter) {
   bool ReturnsVoid = F.getReturnType() == VoidTy;
   // Create a type containing the return value and all args.
   // Since secure functions only support 1 argument we will
@@ -141,21 +177,13 @@ Function *SGXStubify::createAdapter(Function &F) {
     FrameTypes.push_back(Arg.getType());
 
   StructType *FrameTy = StructType::get(*C, FrameTypes);
-  Type *FramePtrTy = PointerType::getUnqual(FrameTy);
 
-  // Create insecure adapter:
   Function *InsecureAdapter =
     Function::Create(F.getFunctionType(), Function::PrivateLinkage,
                      Twine("__llvmsgx_insadapt_") + F.getName());
-  FunctionType *SecureAdapterFTy = FunctionType::get(VoidTy, {FramePtrTy}, false);
-  Function *SecureAdapter =
-    Function::Create(SecureAdapterFTy, Function::PrivateLinkage,
-                     Twine("__llvmsgx_secadapt_") + F.getName());
-
-  F.getParent()->getFunctionList().insert(F.getIterator(), SecureAdapter);
-  F.getParent()->getFunctionList().insert(F.getIterator(), InsecureAdapter);
+  M->getFunctionList().insert(F.getIterator(), InsecureAdapter);
   InsecureAdapter->copyAttributesFrom(&F);
-  InsecureAdapter->removeFnAttr(SGX_SECURE_ATTR);
+  InsecureAdapter->setSection("");
 
   // Add the following code.
   // secure TRet Func(T1 arg1, T2 arg2, ...) {
@@ -165,7 +193,7 @@ Function *SGXStubify::createAdapter(Function &F) {
   //   ...
   //   if (!__llvmsgx_enclave_tcs)
   //     __llvmsgx_enclave_init(&SecureAdapter);
-  //   EENTER(__llvmsgx_enclave_tcs, exception_handler, &f);
+  //   EENTER(__llvmsgx_enclave_tcs, exception_handler, FuncIndex, &f);
   //   return f.ret;
   // }
 
@@ -173,8 +201,7 @@ Function *SGXStubify::createAdapter(Function &F) {
   AllocaInst *FrameAlloc = IRB.CreateAlloca(FrameTy);
   uint32_t InsertIndex = ReturnsVoid ? 0 : 1;
   for (auto &Arg : InsecureAdapter->args()) {
-    Value *StoredArgAddr =
-      IRB.CreateGEP(FrameAlloc, {IRB.getInt32(0), IRB.getInt32(InsertIndex)});
+    Value *StoredArgAddr = IRB.CreateStructGEP(nullptr, FrameAlloc, InsertIndex);
     IRB.CreateStore(&Arg, StoredArgAddr);
     InsertIndex++;
   }
@@ -184,11 +211,12 @@ Function *SGXStubify::createAdapter(Function &F) {
 
   const char *EnterConstraints =
     // First get inputs:
-    // D = rdi <- first arg
-    // b = rbx <- TCS
-    // c = rcx <- AEP
-    // a = rax <- leaf func (EENTER)
-    "{di},{bx},{cx},{ax},"
+    // rdi <- first arg (function index)
+    // rsi <- second arg (pointer to frame)
+    // rbx <- TCS
+    // rcx <- AEP
+    // rax <- leaf func (EENTER)
+    "{di},{si},{bx},{cx},{ax},"
     // Now clobbers. Since this is a call we actually clobber
     // most regs, however here we just take a subset since
     // there is an internal limit on the number of clobbers.
@@ -200,7 +228,7 @@ Function *SGXStubify::createAdapter(Function &F) {
     "~{flags},~{memory}";
 
   FunctionType *EnterFTy =
-    FunctionType::get(VoidTy, {FramePtrTy, Int8PtrTy, Int8PtrTy, Int64Ty}, false);
+    FunctionType::get(VoidTy, {Int64Ty, Int8PtrTy, Int8PtrTy, Int8PtrTy, Int64Ty}, false);
   InlineAsm *EnterIA = InlineAsm::get(EnterFTy, "enclu", EnterConstraints,
                                       /*hasSideEffect*/true,
                                       /*isAlignStack*/false,
@@ -209,15 +237,16 @@ Function *SGXStubify::createAdapter(Function &F) {
   LoadInst *TailLoad = IRB.CreateLoad(EncTcsGlobal);
 
   Constant *ConstEEnter = IRB.getInt64(ENCLU_EENTER);
+  Value *FuncIndexConst = IRB.getInt64(FuncIndex);
+  Value *FrameAsI8Ptr = IRB.CreateBitCast(FrameAlloc, Int8PtrTy);
   CallInst *EnterCall =
-    IRB.CreateCall(EnterIA, {FrameAlloc, TailLoad, EncEH, ConstEEnter});
+    IRB.CreateCall(EnterIA, {FuncIndexConst, FrameAsI8Ptr, TailLoad, EncEH, ConstEEnter});
   EnterCall->addAttribute(AttributeList::FunctionIndex, Attribute::NoUnwind);
 
   if (ReturnsVoid)
     IRB.CreateRetVoid();
   else {
-    Value *RetValAddr =
-      IRB.CreateGEP(FrameAlloc, {IRB.getInt32(0), IRB.getInt32(0)});
+    Value *RetValAddr = IRB.CreateStructGEP(nullptr, FrameAlloc, 0);
 
     Value *RetVal = IRB.CreateLoad(RetValAddr);
     IRB.CreateRet(RetVal);
@@ -226,52 +255,104 @@ Function *SGXStubify::createAdapter(Function &F) {
   Instruction *Then = SplitBlockAndInsertIfThen(TcsIsNull, TailLoad, false);
 
   IRB.SetInsertPoint(Then);
-  IRB.CreateCall(EncInit, {IRB.CreateBitCast(SecureAdapter, Int8PtrTy)});
 
-  // Create secure adapter. Add this code:
-  // void SecureAdapter(FrameTy *f) {
-  //   f->ret = SecureFunc(f->arg1, f->arg2, ...);
-  //   EEXIT();
-  // }
-  SecureAdapter->setSection(SGX_SECURE_SECTION);
-  IRB.SetInsertPoint(BasicBlock::Create(*C, "", SecureAdapter));
+  IRB.CreateCall(EncInit, {IRB.CreateBitCast(&SecureAdapter, Int8PtrTy)});
 
-  SmallVector<Value *, 8> SecureToImplArgs;
-  for (unsigned i = 0; i < F.getFunctionType()->getNumParams(); i++) {
-    unsigned LoadIndex = ReturnsVoid ? i : (1 + i);
-    Value *LoadedArgAddr =
-      IRB.CreateGEP(&*SecureAdapter->arg_begin(),
-                    {IRB.getInt32(0), IRB.getInt32(LoadIndex)});
-    Value *LoadedArg = IRB.CreateLoad(LoadedArgAddr);
-    SecureToImplArgs.push_back(LoadedArg);
-  }
-
-  CallInst *ImplCall = IRB.CreateCall(&F, SecureToImplArgs);
-  if (!ReturnsVoid) {
-    Value *RetValAddr =
-      IRB.CreateGEP(&*SecureAdapter->arg_begin(),
-                    {IRB.getInt32(0), IRB.getInt32(0)});
-    IRB.CreateStore(ImplCall, RetValAddr);
-  }
-
-  FunctionType *ExitFTy = FunctionType::get(VoidTy, {Int64Ty, Int64Ty}, false);
-  InlineAsm *ExitIA = InlineAsm::get(ExitFTy, "enclu",
-                                     // Does not return, so no clobbers necessary
-                                     "{bx},{ax}",
-                                     /*hasSideEffect*/true,
-                                     /*isAlignStack*/false,
-                                     InlineAsm::AD_Intel);
-  Constant *Const0 = IRB.getInt64(0);
-  Constant *ConstEExit = IRB.getInt64(ENCLU_EEXIT);
-
-  CallInst *ExitCall = IRB.CreateCall(ExitIA, {Const0, ConstEExit});
-  ExitCall->addAttribute(AttributeList::FunctionIndex, Attribute::NoUnwind);
-  ExitCall->addAttribute(AttributeList::FunctionIndex, Attribute::NoReturn);
-
-  IRB.CreateRetVoid();
-
-  return InsecureAdapter;
+  return SecureFuncAdapterInfo
+  {
+    FuncIndex,
+    &F,
+    InsecureAdapter,
+    FrameTy,
+  };
 }
+
+// The secure adapter is responsible for unpacking arguments, jumping to the correct
+// secure function, and packing the returned value. It allows us to support arbitrary
+// args and return values (secure functions can only support up to 2 args and no returns),
+// and also allows us multiple entry points (OpenSGX only supports a single TCS).
+// The secure adapter has code looking like the following:
+// void SecureAdapter(int FuncIndex, char *Frame) {
+//   switch (FuncIndex) {
+//   case 0:
+//     Func0FrameTy *Func0Frame = (Func0FrameTy *)Frame;
+//     Func0Frame->Ret = Func0(Func0Frame->Arg1, Func0Frame->Arg2, ...);
+//     break;
+//   case 1:
+//     Func1FrameTy *Func1Frame = (Func1FrameTy *)Frame;
+//     Func1Frame->Ret = Func1(Func1Frame->Arg1, Func1Frame->Arg2, ...);
+//     break;
+//   }
+// }
+// Where a case is generated for each secure function called by insecure code.
+// When an insecure adapter needs to enter its function, it uses EENTER with the
+// proper index and a pointer to a (stack allocated) frame structure containing
+// args and space for the return value.
+void SGXStubify::fillSecureAdapter(Function *SecureAdapter,
+                                   ArrayRef<SecureFuncAdapterInfo> Adapters) {
+  if (Adapters.empty())
+    return;
+
+  SecureAdapter->setSection(SGX_SECURE_SECTION);
+
+  auto ArgIt = SecureAdapter->arg_begin();
+  Value &FuncIndexArg = *ArgIt;
+  ArgIt++;
+  Value &FrameArg = *ArgIt;
+  ArgIt++;
+
+  BasicBlock *SwitchBB = BasicBlock::Create(*C, "secadapt-switch", SecureAdapter);
+  // We always need a default BB. Just mark it unreachable.
+  BasicBlock *SwitchDefaultBB = BasicBlock::Create(*C, "", SecureAdapter);
+
+  IRBuilder<> IRB(SwitchDefaultBB);
+  IRB.CreateUnreachable();
+
+  IRB.SetInsertPoint(SwitchBB);
+  SwitchInst *Switch = IRB.CreateSwitch(&FuncIndexArg, SwitchDefaultBB, Adapters.size());
+
+  for (const auto& AI : Adapters) {
+    BasicBlock *Case = BasicBlock::Create(*C, "", SecureAdapter);
+    Switch->addCase(IRB.getInt64(AI.FuncIndex), Case);
+
+    IRB.SetInsertPoint(Case);
+    // Add this code:
+    //   f->ret = SecureFunc(f->arg1, f->arg2, ...);
+    //   EEXIT();
+    SmallVector<Value *, 8> SecureToImplArgs;
+
+    // Dereference all args from frame
+    Type *RetTy = AI.OriginalFunction->getReturnType();
+    for (unsigned i = 0; i < AI.OriginalFunction->getFunctionType()->getNumParams(); i++) {
+      unsigned ArgIndex = RetTy == VoidTy ? i : (1 + i);
+      Value *LoadedArgAddr = IRB.CreateStructGEP(AI.FrameTy, &FrameArg, ArgIndex);
+      Value *LoadedArg = IRB.CreateLoad(LoadedArgAddr);
+      SecureToImplArgs.push_back(LoadedArg);
+    }
+
+    CallInst *ImplCall = IRB.CreateCall(AI.OriginalFunction, SecureToImplArgs);
+    // Store return into frame
+    if (RetTy != VoidTy) {
+      Value *RetValAddr = IRB.CreateStructGEP(AI.FrameTy, &FrameArg, 0);
+      IRB.CreateStore(ImplCall, RetValAddr);
+    }
+
+    FunctionType *ExitFTy = FunctionType::get(VoidTy, {Int64Ty, Int64Ty}, false);
+    InlineAsm *ExitIA = InlineAsm::get(ExitFTy, "enclu",
+                                       // Does not return, so no clobbers necessary
+                                       "{bx},{ax}",
+                                       /*hasSideEffect*/true,
+                                       /*isAlignStack*/false,
+                                       InlineAsm::AD_Intel);
+    Constant *Const0 = IRB.getInt64(0);
+    Constant *ConstEExit = IRB.getInt64(ENCLU_EEXIT);
+
+    CallInst *ExitCall = IRB.CreateCall(ExitIA, {Const0, ConstEExit});
+    ExitCall->addAttribute(AttributeList::FunctionIndex, Attribute::NoUnwind);
+    ExitCall->addAttribute(AttributeList::FunctionIndex, Attribute::NoReturn);
+
+    IRB.CreateUnreachable();
+  }
 }
 
 char SGXStubify::ID = 0;
